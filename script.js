f73b30792a005c14ae8c795399f37b1870b22e5d
Willy-557 <williamfsetiadi@gmail.com>
2026-05-23 22:25:31 +0700

    Adding new feature on the project showcase and new element

---
+++ script.js
@@ -0,0 +1,9 @@
+function filter(category) {
+    document.querySelectorAll('#kotakbody31').forEach(card => {
+        if (category === 'all' || card.classList.contains(category)) {
+            card.style.display = 'block';
+        } else {
+            card.style.display = 'none';
+        }
+    });
+}
